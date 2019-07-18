1a9c92ed5a9393d1129b7f3000a1282f34581092
snsart <snsart@163.com>
2019-07-18 18:07:05 +0800

    add

--- demo3/es6.js
+++ demo3/es6.js
@@ -18,3 +18,37 @@ function fn() {
 }
 
 `foo ${fn()} bar`
+
+
+/*标签模板*/
+/*将模板字符串紧跟在一个函数名后面，该函数将被调用，来处理这个模板字符串*/
+alert`123`
+// 等同于
+alert(123)
+
+/*当模板字符串里面含有变量时，将先将模板字符串分割为多个参数，再传入函数进行处理，参数分割原则如下：
+ *1. 以变量为分割点，将字符串分为多段,若变量在首位，则第一段字符串为'',若变量在末尾，最后一段字符串为'';
+ *2. 将分割后的多段字符串放在一个数组中，作为函数的第一个参数
+ *3. 变量作为剩余的参数依次排在第一个数组参数后面
+ * 举例：`Hello ${ a + b } world ${ a * b }` 被分割为参数 ['hello','world'],a+b,a*b;
+ * 
+ * */
+
+/*算法：对模板字符串变量中的特殊字符进行转义，转义规则如下：&替换为&amp; <替换为&lt; >替换为&gt;*/
+
+function saferHTML(templateData){
+	let s=templateData[0];
+	for(let i=1;i<arguments.length;i++){
+		let arg=arguments[i];
+		s += arg.replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;");
+        s+=templateData[i];
+	}
+	return s;
+}
+
+let sender = '<script>alert("abc")</script>'; // 恶意代码
+let message = saferHTML`<p>${sender} has sent you a message.</p>`;
+
+console.log(message);
