4e55dce4957ab07a882c2ad83c28adddcee1e14b
snsart <snsart@163.com>
2019-07-26 18:09:56 +0800

    add

---
+++ demo15/es6.js
@@ -0,0 +1,4 @@
+//import {a} from './m1.js';
+import { foo } from 'my_module';
+
+console.log(a);
---
+++ demo15/m1.js
@@ -0,0 +1,5 @@
+export var a="nihao";
+export var b="hello";
+export var c=function(){
+	console.log(c);
+}
